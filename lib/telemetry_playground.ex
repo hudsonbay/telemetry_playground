cbd0f791f6723229844730eab8677dc8f7db5ce9
hudsonbay <manuelmenendezalfonso@gmail.com>
2022-02-13 18:12:18 -0500

    refactor: :fire: Deleted initial code

--- lib/telemetry_playground.ex
+++ lib/telemetry_playground.ex
@@ -2,17 +2,4 @@ defmodule TelemetryPlayground do
   @moduledoc """
   Documentation for `TelemetryPlayground`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> TelemetryPlayground.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/telemetry_playground_test.exs
+++ test/telemetry_playground_test.exs
@@ -1,8 +1,4 @@
 defmodule TelemetryPlaygroundTest do
   use ExUnit.Case
   doctest TelemetryPlayground
-
-  test "greets the world" do
-    assert TelemetryPlayground.hello() == :world
-  end
 end
